daf028b0152849120c93cd3c58f84fefea335128
Aaron Moore <two.a.ron@gmail.com>
2016-01-28 15:23:32 -0800

    Remove accidental inclusion of auto-generated build modifications.

--- www/SocialSharing.js
+++ www/SocialSharing.js
@@ -1,4 +1,4 @@
-﻿cordova.define("cordova-plugin-x-socialsharing.SocialSharing", function(require, exports, module) { var cordova = require('cordova');
+﻿var cordova = require('cordova');
 
 function SocialSharing() {
 }
@@ -129,5 +129,3 @@ SocialSharing.install = function () {
 };
 
 cordova.addConstructor(SocialSharing.install);
-
-});
